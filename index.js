9108c3fb86939b5eab09aab9436466da118152b2
konstantinos <konstantinos.24@hotmail.com>
2022-07-31 21:25:58 +0300

    fe

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const path = require('path');
 const app = express();
 
 // app.use(express.static(path.join(__dirname + 'public')));
+app.use(cors());
 app.use(express.static('client'));
 app.use(
   session({
