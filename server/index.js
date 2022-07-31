9e8ebc3bf0a0c27e5d5bd2ab44480999ff198954
konstantinos <konstantinos.24@hotmail.com>
2022-07-31 09:36:54 +0300

    ff

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const upload = multer();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 const app = express();
 
