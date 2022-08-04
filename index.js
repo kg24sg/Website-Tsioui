de5bd4c4c73fef30d40284f2249ebbd14c4ec11f
konstantinos <konstantinos.24@hotmail.com>
2022-08-04 23:01:19 +0300

    make it better

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ const path = require('path');
 const app = express();
 
 // app.use(express.static(path.join(__dirname + 'public')));
-app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('./client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.use(
   session({
     secret: 'dont mess with tsiouis website',
