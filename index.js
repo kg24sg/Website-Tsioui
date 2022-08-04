1fe517b6176dc2c641285ad1cefd866bd6e0ebc4
konstantinos <konstantinos.24@hotmail.com>
2022-08-04 23:04:49 +0300

    make it better

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 // app.use(express.static(path.join(__dirname + 'public')));
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('./client/build'));
+  app.use(express.static('../client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
