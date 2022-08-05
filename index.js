9fc231a321af17718a79164ee3a0a4bbf757eb1b
konstantinos <konstantinos.24@hotmail.com>
2022-08-05 07:27:08 +0300

    make it better

--- index.js
+++ index.js
@@ -16,16 +16,15 @@ const path = require('path');
 const app = express();
 
 // app.use(express.static(path.join(__dirname + 'public')));
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('../client/build'));
+// Exprees will serve up production assets
+app.use(express.static('../client/build'));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// Express serve up index.html file if it doesn't recognize route
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.use(
   session({
     secret: 'dont mess with tsiouis website',
