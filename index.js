67ba678f7d351fae4dd0ec42a449a0b82d71de95
konstantinos <konstantinos.24@hotmail.com>
2022-08-05 20:52:40 +0300

    make it better

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.static('../client/build'));
 // Express serve up index.html file if it doesn't recognize route
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname + ' client' + 'build' + 'index.html'));
+  res.sendFile(path.resolve('../client/build'));
 });
 
 app.use(
