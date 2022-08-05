ee03b65393ada6106380e30123d9b5275d67b86d
konstantinos <konstantinos.24@hotmail.com>
2022-08-05 20:49:46 +0300

    make it better

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 // app.use(express.static(path.join(__dirname + 'public')));
 // Exprees will serve up production assets
-app.use(express.static('client/build'));
+app.use(express.static('../client/build'));
 // Express serve up index.html file if it doesn't recognize route
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname + ' client' + 'build' + 'index.html'));
