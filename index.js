d5d1dbdef755e59d0833189fc734196a4924bb31
konstantinos <konstantinos.24@hotmail.com>
2023-05-28 07:32:56 +0300

    gg

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV == 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('./client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
