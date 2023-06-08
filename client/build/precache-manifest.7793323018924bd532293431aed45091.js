9b8ae5a551542a4ce84c1ff7fcc52f427e0bb404
konstantinos <konstantinos.24@hotmail.com>
2023-06-08 23:18:21 +0300

    deploy

---
+++ client/build/precache-manifest.7793323018924bd532293431aed45091.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bfd46c1b3cb100f7b0e",
+    "url": "/static/css/main.b17dd518.chunk.css"
+  },
+  {
+    "revision": "9bfd46c1b3cb100f7b0e",
+    "url": "/static/js/main.9bfd46c1.chunk.js"
+  },
+  {
+    "revision": "8e0ee6a8b8e321779f24",
+    "url": "/static/js/1.8e0ee6a8.chunk.js"
+  },
+  {
+    "revision": "ec2f775c6157c13c7faf",
+    "url": "/static/css/2.5fcc6504.chunk.css"
+  },
+  {
+    "revision": "ec2f775c6157c13c7faf",
+    "url": "/static/js/2.ec2f775c.chunk.js"
+  },
+  {
+    "revision": "69639397558084bc9688",
+    "url": "/static/js/runtime~main.69639397.js"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "e65576146287220a703bce1d2dfc4342",
+    "url": "/index.html"
+  }
+];
