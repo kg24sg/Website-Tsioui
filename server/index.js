5a46eb5508445f6fb4f2d760c29390c978df86e1
konstantinos <konstantinos.24@hotmail.com>
2023-06-10 08:57:46 +0300

    f

--- server/index.js
+++ server/index.js
@@ -168,9 +168,9 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV == 'production') {
-  app.use(express.static(path.join(__dirname, '../client', 'build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
 
